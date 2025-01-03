647bf3eeb685af6e33299a854bb0cf05d0212e23
sco <scottmathiasrichards@gmail.com>
2025-01-03 21:44:13 +0100

    7

--- 7.lua
+++ 7.lua
@@ -1,24 +1,33 @@
-local ret = 0
-
-local primes = { 1, 2, 3 }
-
 local function isPrime(n)
-	for _, p in pairs(primes) do
-		if n % p == 0 then
+	if n % 2 == 0 or n % 3 == 0 then
+		return false
+	end
+
+	local curr = 5
+	local max = math.sqrt(n)
+
+	while curr <= max do
+		if n % curr == 0 then
 			return false
 		end
+		curr = curr + 2
 	end
+
+	return true
 end
 
+local ret = 0
 local val = 3
+local count = 2
 
-while table.getn(primes) ~= 10 do
+while count < 10001 do
 	val = val + 2
 	if isPrime(val) then
-		table.insert(primes, val)
+		count = count + 1
+	end
+	if count == 10001 then
+		ret = val
 	end
 end
-
-ret = primes[10001]
 
 print(ret)
