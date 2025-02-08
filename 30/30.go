ac756019fff0a4164caadd3967ce051ca7f7ec28
sco <scottmathiasrichards@gmail.com>
2025-02-08 01:03:15 +0100

    30

--- 30/30.go
+++ 30/30.go
@@ -6,23 +6,36 @@ import (
 )
 
 func main() {
-	pow := func(n, p int) int {
-		if p == 0{
-			return 1
-		}
-
-		ret := n
-		for 
-	}
-
 	isSumOfFifthPower := func(n int) bool {
 		s := strconv.Itoa(n)
 		nums := strings.Split(s, "")
 		sum := 0
 		for _, v := range nums {
 			val, _ := strconv.Atoi(v)
+			sum += (val * val * val * val * val)
+			if sum > n {
+				return false
+			}
+		}
+		return sum == n
+	}
 
-			sum += val * *5
+	ret := 0
+
+	pow9 := 9 * 9 * 9 * 9 * 9
+
+	s := pow9
+	m := 1
+
+	for m < s {
+		s += pow9
+		m *= 10
+	}
+
+	for i := 2; i < m; i++ {
+		if isSumOfFifthPower(i) {
+			ret += i
 		}
 	}
+	println(ret)
 }
