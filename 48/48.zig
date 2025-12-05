2c2cef8c6ca1c76c5fe89339bbbc64bf7b79dc1c
rtocs <scottmathiasrichards@gmail.com>
2025-12-05 08:38:18 +0000

    48

---
+++ 48/48.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var count: i64 = 0;
+    var res: i64 = 0;
+
+    for (0..1000) |i| {
+        count += 1;
+        var curr_num = count;
+        const curr_power = count;
+        for (0..(i)) |_| {
+            curr_num = @mod((curr_num * curr_power), 10000000000);
+        }
+        res = @mod(res + curr_num, 10000000000);
+    }
+    std.debug.print("{d}", .{res});
+}
